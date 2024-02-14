de15b79abb3365c98cbde93f9d26b97e93c5db3d
AlexUndrGo <alex.undr.b@gmail.com>
2024-02-14 14:19:53 +0300

    Ansible-3 fix-2

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -8,7 +8,11 @@ variable "app_disk_image" {
 variable "subnet_id" {
   description = "Subnets for modules"
 }
-variable "db_url" {
-  description = "Url for DB mongo"
-}
+#variable "db_url" {
+#  description = "Url for DB mongo"
+#}
 
+#variable "allow_tcp_ports" {
+#  description = "Enter TCP ports to allow connections to (22, 80 by default)"
+#  default     = [22, 80]
+#}
